7407d0fd4fec4a740c9a9faf7bae1f74802964b7
Eduardo Alcaraz <ealcaraz@itmorelia.edu.mx>
2017-03-05 14:22:07 -0600

    recursividad

--- hola.c
+++ hola.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
+  
   printf("Hola mundo utilizando magit\n");
   printf("una nueva rama utilizando magit \n")
-
+  printf("Recursividad demo \n");
   return 0;
+
 }
